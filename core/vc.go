5b9a2a3e7e700a153d2e192bfc48bdc6eb0de30d
comnics <comnics@reappay.co.kr>
2022-03-24 14:10:23 +0900

    Define simple VC struct.

--- core/vc.go
+++ core/vc.go
@@ -1,6 +1,30 @@
 package core
 
-type VCManager struct {
+type VC struct {
 	// Mendatory
 	Context []string `json:"@context"`
+
+	Id           string   `json:"id"`
+	Type         []string `json:"type"`
+	Issuer       string   `json:"issuer"`
+	IssuanceDate string   `json:"issuanceDate"`
+
+	CredentialSubject struct {
+		Id       string `json:"id"`
+		AlumniOf struct {
+			Id   string `json:"id"`
+			Name []struct {
+				value string `json:"value"`
+				lang  string `json:"lang"`
+			} `json:"name"`
+		}
+	} `json:"credentialSubject"`
+
+	Proof struct {
+		Type               string `json:"type"`
+		Created            string `json:"created"`
+		ProofPurpose       string `json:"proofPurpose"`
+		VerificationMethod string `json:"verificationMethod"`
+		Jws                string `json:"jws"`
+	} `json:"proof"`
 }
