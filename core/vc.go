ed23a522693ab5f8e02ff286e30f662b8b45ed58
comnic <comnics@gmail.com>
2022-03-26 13:29:22 +0900

    VC JWT create and verify.

--- core/vc.go
+++ core/vc.go
@@ -2,7 +2,17 @@
 
 package core
 
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
 // https://www.w3.org/TR/vc-data-model
+// https://www.w3.org/TR/vc-data-model/#json-web-token
 type VC struct {
 	// Mendatory
 	Context []string `json:"@context"`
@@ -21,4 +31,74 @@ type Proof struct {
 	ProofPurpose       string `json:"proofPurpose"`
 	VerificationMethod string `json:"verificationMethod"`
 	Jws                string `json:"jws"`
+}
+
+// JWT를 위한 claim
+type JwtClaims struct {
+	jwt.StandardClaims
+
+	Nonce string
+	Vc    VC `json:"vc"`
+}
+
+type VCInterface interface {
+	Sign()
+	Verify() bool
+}
+
+// VC를 JTW로 생성하고 string으로 반환한다.
+func (vc *VC) CreateJWT(pvKey *ecdsa.PrivateKey) string {
+
+	aud := ""
+	exp := time.Now().Add(time.Minute * 5).Unix()
+	jti := "089a411f-0d88-450f-8cc0-1a3acfebecd3"
+	iat := time.Now().Unix()
+	nbf := iat
+	iss := "http://google.com/issuer"
+	sub := ""
+
+	jwtClaims := JwtClaims{
+		jwt.StandardClaims{
+			Audience:  aud,
+			ExpiresAt: exp,
+			Id:        jti,
+			IssuedAt:  iat,
+			Issuer:    iss,
+			NotBefore: nbf,
+			Subject:   sub,
+		},
+		"qwasd!234",
+		*vc,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwtClaims)
+
+	tokenString, err := token.SignedString(pvKey)
+
+	if err != nil {
+
+	}
+
+	return tokenString
+}
+
+func ParseJwt(tokenString string) {
+	token, err := jwt.ParseWithClaims(tokenString, &JwtClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("AllYourBase"), nil
+	})
+
+	if claims, ok := token.Claims.(*JwtClaims); ok && token.Valid {
+		fmt.Printf("%v %v", claims.Vc, claims.Issuer)
+	} else {
+		fmt.Println(err)
+	}
+
+}
+
+func VerifyJwt(token string, pbKey *ecdsa.PublicKey) (bool, error) {
+	parts := strings.Split(token, ".")
+	err := jwt.SigningMethodES256.Verify(strings.Join(parts[0:2], "."), parts[2], pbKey)
+	if err != nil {
+		return false, nil
+	}
+	return true, nil
 }
--- main.go
+++ main.go
@@ -43,7 +43,52 @@ func main() {
 		},
 	}
 	didDocument := core.NewDIDDocument(did.String(), didDocumentAuth)
-	fmt.Printf("DID Document: %s", didDocument)
+	fmt.Printf("DID Document: %s\n", didDocument)
+
+	// @@@@@@@@@@@@@@@@@@@
+
+	myVC := &core.VC{
+		Context: []string{
+			"https://www.w3.org/2018/credentials/v1",
+			"https://www.w3.org/2018/credentials/v2",
+		},
+		Id:           "http://example.edu/credentials/1872",
+		Type:         []string{"VerifiableCredential", "AlumniCredential"},
+		Issuer:       "https://example.edu/issuers/565049",
+		IssuanceDate: "2010-01-01T19:23:24Z",
+		CredentialSubject: map[string]interface{}{
+			"id": "1234567890",
+			"alumniOf": map[string]interface{}{
+				"id": "1234567",
+				"name": []map[string]string{
+					{
+						"value": "Example University",
+						"lang":  "en",
+					}, {
+						"value": "Exemple d'Université",
+						"lang":  "fr",
+					},
+				},
+			},
+		},
+		Proof: core.Proof{
+			Type:               "RsaSignature2018",
+			Created:            "2017-06-18T21:19:10Z",
+			ProofPurpose:       "assertionMethod",
+			VerificationMethod: "https://example.edu/issuers/565049#key-1",
+			Jws:                "eyJhbGciOiJSUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..TCYt5XsITJX1CxPCT8yAV-TVkIEq_PbChOMqsLfRoPsnsgw5WEuts01mq-pQy7UJiN5mgRxD-WUcX16dUEMGlv50aqzpqh4Qktb3rk-BuQy72IFLOqV0G_zS245-kronKb78cPN25DGlcTwLtjPAYuNzVBAh4vGHSrQyHUdBBPM",
+		},
+	}
+	token := myVC.CreateJWT(ecdsa.PrivateKey)
+	fmt.Printf("\nVC JWT Token: %s\n", token)
+
+	res, _ := core.VerifyJwt(token, ecdsa.PublicKey)
+
+	if res {
+		fmt.Println("VC is verified.")
+	} else {
+		fmt.Println("VC is Not verified.")
+	}
 
 	//// protobuf test
 	//testDid := pb.Did{Id: "12342", Desc: "abcde"}
