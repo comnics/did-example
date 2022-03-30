8063be1c62c0fc66dca5c964a0bd8015356a5341
comnic <comnics@gmail.com>
2022-03-30 10:27:24 +0900

    Refactoring.

--- examples/cryptograph/rsa/main.go
+++ examples/cryptograph/rsa/main.go
@@ -8,54 +8,54 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"fmt"
+
+	"github.com/comnics/did-example/core"
 )
 
 func main() {
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048) // 개인 키와 공개 키 생성
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	publicKey := &privateKey.PublicKey // 개인 키 변수 안에 공개 키가 들어있음
-	fmt.Printf("PbKey: %x\n", publicKey)
+	rsam, err := core.NewRsa()
 
-	pbkey_byte := x509.MarshalPKCS1PublicKey(publicKey)
-	fmt.Printf("PbKey byte: %s\n", pbkey_byte)
-	pbkey_base64 := base64.StdEncoding.EncodeToString(pbkey_byte)
-	fmt.Printf("PbKey: %s\n", pbkey_base64)
+	fmt.Printf("PbKey: %x\n", rsam.PublicKey)
 
-	pbkey_byte2, err := base64.StdEncoding.DecodeString(pbkey_base64)
-	fmt.Printf("PbKey byte: %s\n", pbkey_byte2)
+	pbKeyByte := x509.MarshalPKCS1PublicKey(rsam.PublicKey)
+	fmt.Printf("PbKey byte: %s\n", pbKeyByte)
+	pbKeyBase64 := base64.StdEncoding.EncodeToString(pbKeyByte)
+	fmt.Printf("PbKey: %s\n", pbKeyBase64)
 
-	pub, err := x509.ParsePKCS1PublicKey(pbkey_byte2)
+	pbKeyByte2, err := base64.StdEncoding.DecodeString(pbKeyBase64)
+	fmt.Printf("PbKey byte: %s\n", pbKeyByte2)
+
+	pub, err := x509.ParsePKCS1PublicKey(pbKeyByte2)
 	fmt.Printf("PbKey: %x\n", pub)
 
-	message := "안녕하세요. Go 언어"
-	hash := sha256.New()        // 해시 인스턴스 생성
-	hash.Write([]byte(message)) // 해시 인스턴스에 문자열 추가
-	digest := hash.Sum(nil)     // 문자열의 MD5 해시 값 추출
+	message := "Hello World."
+	hash := sha256.New()
+	hash.Write([]byte(message))
+	digest := hash.Sum(nil)
 
 	var h1 crypto.Hash
-	signature, err := rsa.SignPKCS1v15( // 개인 키로 서명
+	//개인키로 서명.
+	signature, err := rsa.SignPKCS1v15(
 		rand.Reader,
-		privateKey, // 개인 키
+		rsam.PrivateKey,
 		h1,
-		digest, //
+		digest,
 	)
 
 	fmt.Println(base64.StdEncoding.EncodeToString(signature))
 
 	var h2 crypto.Hash
-	err = rsa.VerifyPKCS1v15( // 공개 키로 서명 검증
-		pub, // 공개 키
+	// 공개키로 서명 검증.
+	err = rsa.VerifyPKCS1v15(
+		pub,
 		h2,
-		digest,    //
-		signature, // 서명 값
+		digest,
+		signature,
 	)
 
 	if err != nil {
-		fmt.Println("검증 실패")
+		fmt.Println("Fail verification.")
 	} else {
-		fmt.Println("검증 성공")
+		fmt.Println("Success verification")
 	}
 }
