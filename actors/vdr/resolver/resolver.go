35d2f4179370f0b7fab5a4e4682b897309de5490
comnic <comnics@gmail.com>
2022-04-06 00:00:56 +0900

    Add Resolver.

---
+++ actors/vdr/resolver/resolver.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/comnics/did-example/protos"
+	"github.com/syndtr/goleveldb/leveldb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type resolverServer struct {
+	protos.UnimplementedResolverServer
+}
+
+func byte2string(b []byte) string {
+	return string(b[:len(b)])
+}
+
+func (server *resolverServer) ResolveDid(ctx context.Context, req *protos.ResolverRequest) (*protos.ResolverResponse, error) {
+	db, err := leveldb.OpenFile("did_db/dids", nil)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	data, err := db.Get([]byte(req.Did), nil)
+	didDocument := byte2string(data)
+	return &protos.ResolverResponse{DidDocument: didDocument}, nil
+}
+
+func main() {
+	fmt.Println("### Start Resolver ###")
+	lis, err := net.Listen("tcp", "0.0.0.0:9001")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := resolverServer{}
+	s := grpc.NewServer()
+	protos.RegisterResolverServer(s, &server)
+
+	log.Printf("Resolver Server is listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
