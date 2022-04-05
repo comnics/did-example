84c5e19663ba57bbfa42d386cd105f3dc0851a42
comnic <comnics@gmail.com>
2022-04-06 00:00:33 +0900

    Add Registrar.

---
+++ actors/vdr/registrar/registrar.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/comnics/did-example/protos"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+type registrarServer struct {
+	protos.UnimplementedRegistrarServer
+}
+
+func (server *registrarServer) RegisterDid(ctx context.Context, req *protos.RegistrarRequest) (*protos.RegistrarResponse, error) {
+	db, err := leveldb.OpenFile("did_db/dids", nil)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Put([]byte(req.Did), []byte(req.DidDocument), nil)
+
+	return &protos.RegistrarResponse{Result: "OK"}, nil
+
+}
+
+func main() {
+	fmt.Println("### Start Registrar ###")
+	lis, err := net.Listen("tcp", "0.0.0.0:9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := registrarServer{}
+	s := grpc.NewServer()
+	protos.RegisterRegistrarServer(s, &server)
+
+	log.Printf("Registrar Server is listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
