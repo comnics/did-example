b7271af550b091f231fab484ba53621ef41ca141
comnic <comnics@gmail.com>
2022-04-09 12:49:38 +0900

    Add jwt functions.

--- core/jwt.go
+++ core/jwt.go
@@ -62,3 +62,66 @@ func ParseAndVerifyJwtForVC(tokenString string) (bool, *JwtClaims, error) {
 
 	return false, nil, err
 }
+
+func ParseAndVerifyJwtForVP(tokenString string) (bool, *JwtClaimsForVP, error) {
+	//개별적으로 내부 VC들을 다시 다 검증해야 한다.
+	parseToken, _ := jwt.ParseWithClaims(tokenString, &JwtClaimsForVP{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+			log.Fatalln("unexpected signing method.")
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		//DID를 추출한다.
+		parts := strings.Split(token.Header["kid"].(string), "#")
+		did := parts[0]
+
+		//Resolve한다.
+		didDocumentStr, err := ResolveDid(did)
+		if err != nil {
+			log.Printf("Failed to Resolve DID.\nError: %x\n", err)
+		}
+
+		//Json string을 DID Document 객체로 생성한다.
+		didDocument, err := NewDIDDocumentForString(didDocumentStr)
+		if err != nil {
+			log.Printf("Failed generate DID Document from string.\nError: %x\n", err)
+		}
+		// 첫 번째를 사용한다고 가정한다.
+		// TODO: 키 ID(위의 kid)에 해당하는 키 값 구하기.
+		pbKeyBaseMultibase := didDocument.VerificationMethod[0].PublicKeyMultibase
+		_, bytePubKey, err := multibase.Decode(pbKeyBaseMultibase)
+		pbKey, err := x509.ParsePKIXPublicKey(bytePubKey)
+
+		return pbKey, nil
+	})
+
+	fmt.Println("parseToken: ", parseToken)
+	claims, ok := parseToken.Claims.(*JwtClaimsForVP)
+	fmt.Println("claims: ", claims)
+
+	if ok && parseToken.Valid {
+		if &claims.Vp != nil {
+			vpMapClaims := claims.Vp
+			vcList := vpMapClaims.VerifiableCredential
+
+			for idx, vcToken := range vcList {
+				//TODO: 각각의 VC를 다시 검증해야 한다.
+				fmt.Printf("VC[%d]: %s", idx, vcToken)
+				verify, _, err := ParseAndVerifyJwtForVC(vcToken)
+				if !verify || err != nil {
+					log.Printf("Failed to verify VC[%d] in VP.", idx)
+					// VC가 한 건이라도 오류라면 바로 리턴해 버린다.
+					return false, nil, err
+				}
+				fmt.Printf("	==> VC[%d] is Verified.\n", idx)
+			}
+		} else {
+			return false, nil, fmt.Errorf("VC is not exist.")
+		}
+
+	} else {
+		return false, nil, fmt.Errorf("VP is not valid.")
+	}
+
+	return true, claims, nil
+}
