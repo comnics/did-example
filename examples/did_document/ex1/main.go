b651c3667ae9e7a4ae14fe6ace8777990b8c6037
comnic <comnics@gmail.com>
2022-04-16 13:13:33 +0900

    Add did-document example.

---
+++ examples/did_document/ex1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/comnics/did-example/core"
+	"log"
+)
+
+func main() {
+	var method = "comnic"
+
+	kms := new(core.ECDSAManager)
+	kms.Generate()
+
+	did, err := core.NewDID(method, kms.PublicKeyMultibase())
+
+	if err != nil {
+		log.Printf("Failed to generate DID, error: %v\n", err)
+	}
+
+	// DID Document 생성.
+	verificationId := fmt.Sprintf("%s#keys-1", did)
+	verificationMethod := []core.VerificationMethod{
+		{
+			Id:                 verificationId,
+			Type:               "EcdsaSecp256k1VerificationKey2019",
+			Controller:         did.String(),
+			PublicKeyMultibase: kms.PublicKeyMultibase(),
+		},
+	}
+	didDocument := core.NewDIDDocument(did.String(), verificationMethod)
+
+	fmt.Println("### New DID ###")
+	fmt.Printf("did => %s\n", did)
+	fmt.Printf("did document => %+v\n", didDocument)
+
+}
