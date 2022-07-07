22c33645a9775fa35a4c02de33048ad14bff673f
Minho LEE <comnics@gmail.com>
2022-07-07 15:05:33 +0900

    Modify resolver grpc-gateway

--- actors/vdr/resolver/grpc-gateway/main.go
+++ actors/vdr/resolver/grpc-gateway/main.go
@@ -21,7 +21,7 @@ func main() {
 
 	fmt.Println("### Start Resover RESTful ###")
 
-	if err := protos.RegisterRegistrarHandlerFromEndpoint(
+	if err := protos.RegisterResolverHandlerFromEndpoint(
 		ctx,
 		mux,
 		config.SystemConfig.ResolverAddr,
